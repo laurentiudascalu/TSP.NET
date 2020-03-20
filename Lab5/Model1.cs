c160b5096f7b8934b52c150bd683c5a94920fc3d
laurentiudascalu <48380374+laurentiudascalu@users.noreply.github.com>
2020-03-20 20:59:03 +0200

    Add files via upload

---
+++ Lab5/Model1.cs
@@ -0,0 +1,177 @@
+namespace WindowsFormsApp1
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    public class ModelSelfRefrences : DbContext
+    {
+        public ModelSelfRefrences()
+        : base("name=Model1")
+        {
+        }
+        public virtual DbSet<SelfReference> SelfReferences { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<SelfReference>()
+            .HasMany(m => m.References)
+            .WithOptional(m => m.ParentSelfReference);
+        }
+    }
+
+    public class SelfReference
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SelfReferenceId { get; private set; }
+        public string Name { get; set; }
+        public int? ParentSelfReferenceId { get; private set; }
+        [ForeignKey("ParentSelfReferenceId")]
+        public SelfReference ParentSelfReference { get; set; }
+        public virtual ICollection<SelfReference> References { get; set; }
+        public SelfReference()
+        {
+            References = new HashSet<SelfReference>();
+        }
+        public SelfReference(string name) : this()
+        {
+            Name = name;
+        }
+
+        public void AddReference(List<SelfReference> refs)
+        {
+            foreach (var r in refs)
+            {
+                r.ParentSelfReferenceId = SelfReferenceId;
+                r.ParentSelfReference = ParentSelfReference;
+                References.Add(r);
+            }
+        }
+    }
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public ProductContext() : base("name=Model1")
+        { }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>()
+            .Map(m =>
+            {
+                m.Properties(p => new { p.SKU, p.Description, p.Price });
+                m.ToTable("Product", "BazaDeDate");
+            })
+            .Map(m =>
+            {
+                m.Properties(p => new { p.SKU, p.ImageURL });
+                m.ToTable("ProductWebInfo", "BazaDeDate");
+            });
+        }
+    }
+
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int SKU { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageURL { get; set; }
+    }
+    public class PhotographContext : DbContext
+    {
+        public DbSet<Photograph> Photographs { get; set; }
+        public DbSet<PhotographFullImage> PhotographFullImages { get; set; }
+        public PhotographContext() : base("name=Model1")
+        { }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Photograph>()
+            .HasRequired(p => p.PhotographFullImage)
+            .WithRequiredPrincipal(p => p.Photograph);
+            modelBuilder.Entity<Photograph>()
+            .ToTable("Photograph", "BazaDeDate");
+            modelBuilder.Entity<PhotographFullImage>()
+            .ToTable("Photograph", "BazaDeDate");
+        }
+    }
+    public class Photograph
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PhotoId { get; set; }
+        public string Title { get; set; }
+        public byte[] ThumbnailBits { get; set; }
+        [ForeignKey("PhotoId")]
+        public virtual PhotographFullImage PhotographFullImage { get; set; }
+    }
+    public class PhotographFullImage
+    {
+        [Key]
+        public int PhotoId { get; set; }
+        public byte[] HighResolutionBits { get; set; }
+        [ForeignKey("PhotoId")]
+        public virtual Photograph Photograph { get; set; }
+    }
+    public class BusinessContext : DbContext
+    {
+        public DbSet<Business> Businesses { get; set; }
+        public BusinessContext() : base("name=Model1")
+        { }
+    }
+    public class Business
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BusinessId { get; protected set; }
+        public string Name { get; set; }
+        public string LicenseNumber { get; set; }
+    }
+    public class eCommerce : Business
+    {
+        public string URL { get; set; }
+    }
+    public class Retail : Business
+    {
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZIPCode { get; set; }
+    }
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public EmployeeContext() : base("name=Model1")
+        { }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Employee>()
+            .Map<FullTimeEmployee>(m => m.Requires("EmployeeType")
+            .HasValue(1))
+            .Map<HourlyEmployee>(m => m.Requires("EmployeeType")
+            .HasValue(2));
+        }
+    }
+    public abstract class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EmployeeId { get; protected set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public class FullTimeEmployee : Employee
+    {
+        public decimal? Salary { get; set; }
+    }
+    public class HourlyEmployee : Employee
+    {
+        public decimal? Wage { get; set; }
+    }
+}
